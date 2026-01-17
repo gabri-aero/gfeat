129830a01a65ad4f93a8f6b1b8f8b98875507637
Gabriel Valles <gvallesv@outlook.es>
2026-01-17 18:29:52 +0100

    Incorporated accelerometer noise in GPS observations

--- include/gfeat/observation/GPS.hpp
+++ include/gfeat/observation/GPS.hpp
@@ -26,8 +26,45 @@ public:
         : MultiObservation(l_max, Nr, Nd,
                            get_observations(l_max, I, Nr, Nd, we_0, wo_0)) {}
 
+    void set_observation_error(double sigma_u, double sigma_v, double sigma_w,
+                               std::function<double(double)> ddu_asd,
+                               std::function<double(double)> ddv_asd,
+                               std::function<double(double)> ddw_asd) {
+        // Radial
+        auto radial = this->observations.at(0);
+        radial->set_observation_error(
+            [radial, ddu_asd, ddv_asd, sigma_u](double f) -> double {
+                double w = 2 * M_PI * f;
+                double n = radial->get_wo_dot();
+                double H_ddu_u = abs(w / (w * (n * n - w * w)));
+                double H_ddv_u = abs(2 * n / (w * (n * n - w * w)));
+                return sqrt(pow(sigma_u, 2) + pow(H_ddu_u * ddu_asd(f), 2) +
+                            pow(H_ddv_u * ddv_asd(f), 2));
+            });
+        // Along-track
+        auto along = this->observations.at(1);
+        along->set_observation_error(
+            [along, ddu_asd, ddv_asd, sigma_v](double f) -> double {
+                double w = 2 * M_PI * f;
+                double n = along->get_wo_dot();
+                double H_ddu_v = abs(2 * n * w / (w * w * (n * n - w * w)));
+                double H_ddv_v =
+                    abs((w * w + 3 * n * n) / (w * w * (n * n - w * w)));
+                return sqrt(pow(sigma_v, 2) + pow(H_ddu_v * ddu_asd(f), 2) +
+                            pow(H_ddv_v * ddv_asd(f), 2));
+            });
+        // Cross-track
+        auto cross = this->observations.at(2);
+        cross->set_observation_error(
+            [cross, ddw_asd, sigma_w](double f) -> double {
+                double w = 2 * M_PI * f;
+                double n = cross->get_wo_dot();
+                double H_ddw_w = abs(1 / (n * n - w * w));
+                return sqrt(pow(sigma_w, 2) + pow(H_ddw_w * ddw_asd(f), 2));
+            });
+    }
     void set_observation_error(std::function<double(double)> asd) {
-        for (auto &observation : observations) {
+        for (auto &observation : this->observations) {
             observation->set_observation_error(asd);
         }
     }
